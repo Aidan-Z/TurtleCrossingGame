08f7e5f04ee52ef315faf08361ab495e65d2c03f
Aidan-Z <aidanzamfir@gmail.com>
2023-05-09 10:08:17 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+
+
+
+
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+    screen.exitonclick()
+
+
+if __name__ == '__main__':
+    main()
