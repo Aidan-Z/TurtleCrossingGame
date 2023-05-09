5ae2afb9f1bf6d8c1f3fdf00f6ba1efe5c7a9cb8
Aidan-Z <aidanzamfir@gmail.com>
2023-05-09 13:05:45 -0300

    added player class

--- cars.py
+++ cars.py
@@ -1,11 +1,22 @@
 from turtle import Turtle
 import random
 COLORS = ['orange', 'green', 'blue', 'red', 'pink']
+Y_RANGE = range(-295, 260)
+
 
 class Car(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.penup()
         self.shape('square')
         self.shapesize(stretch_wid=1.5, stretch_len=3)
         self.color(random.choice(COLORS))
+        self.start = self.goto(290, random.choice(Y_RANGE))
+
+
+    def move(self):
+        self.backward(5)
+
+
+
--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 from turtle import Screen
 from cars import Car
+from player import Player
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+cars = []
+player = Player()
 
-
-
+screen.listen()
+screen.onkey(player.up, "Up")
+screen.onkey(player.down, "Down")
 
 
 
 def main():
-    screen.update()
+    for i in range(15): #need to creat timer so they generate every x seconds, random range number
+        new_car = Car()
+        cars.append(new_car)
+
+    game_on = True
+    while game_on:
+        time.sleep(0.1)
+        screen.update()
+
+        for i in cars:
+            i.move()
 
 
 
