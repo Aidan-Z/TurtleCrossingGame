39e604804d0e596d75345a36ef3a4f35178fcced
Aidan-Z <aidanzamfir@gmail.com>
2023-05-10 11:12:10 -0300

    .

--- cars.py
+++ cars.py
@@ -5,23 +5,32 @@ Y_RANGE = range(-240, 260)
 X_RANGE = range(-295, 260)
 
 
+
 class Car(Turtle):
 
     def __init__(self):
         super().__init__()
-        self.penup()
-        self.speed(0)
-        self.shape('square')
-        self.shapesize(stretch_wid=1.2, stretch_len=2)
-        self.color(random.choice(COLORS))
-        self.start = self.goto(270, random.choice(Y_RANGE))
+        self.cars = [] #can use list from main because is class attribute
+        self.hideturtle()
 
 
     def move(self):
-        self.backward(5)
+        for i in self.cars:
+            i.backward(5)
 
-    def first_cars(self):
-        self.goto(random.choice(X_RANGE), random.choice(Y_RANGE))
+    def gen_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle('square')
+            new_car.shapesize(stretch_wid=1.2, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.goto(270, random.choice(Y_RANGE))
+            self.cars.append(new_car)
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -5,69 +5,48 @@ from score import Score
 import time
 import threading
 
-#objects
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+new_car = Car()
 player = Player()
-
-s = Score()
-
+score = Score()
 
 
 cars = []
 
 
-
-
-
 screen.listen()
 screen.onkey(player.up, "Up")
 
-def gen_cars():
-    """creates new car objects from class Car()"""
-    new_car = Car()
-    cars.append(new_car)
-
-def first_cars():
-    """generates first 20 car objects from class Car()"""
-    for i in range(20):
-        new_car = Car()
-        new_car.first_cars()
-        cars.append(new_car)
 
 def main():
-    s.game_level()
-    first_cars()
-
+    score.game_level()
 
     game_on = True
     while game_on:
         time.sleep(0.1)
         screen.update()
-
-
-
-
-
-
-
+        new_car.gen_car()
+        # cars.append(new_car)
+        new_car.move()
 
 
 
         #new level:
-        if player.distance(0, 270) <10:
+        if player.distance(0, 280) <10:
             player.next_level()
-            s.new_level()
+            score.new_level()
 
 
-        #move cars across screen:
-        for i in cars:
-            i.move()
-            if player.distance(i) < 34:
+        #check distance of player from car
+        for i in new_car.cars: #accessing class attribute (list of stored car objects)
+            if player.distance(i) < 35:
                 game_on = False
                 print("end")
+
 
 
 
